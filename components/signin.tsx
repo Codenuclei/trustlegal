5397d047ecf589c53428978fa4276a2ba69e87db
codenuclei <abhishekghosh.air1@gmail.com>
2025-03-08 09:35:13 +0530

    new

--- components/signin.tsx
+++ components/signin.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { signIn } from "next-auth/react";
 import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
